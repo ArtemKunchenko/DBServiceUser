c479d1a889321aae36d7ea5741eb53f75731aab8
Артем Кунченко <kunchenko911@ukr.net>
2023-07-02 21:50:29 +0300

    Add project files.

---
+++ DBServiceUser/Source.cpp
@@ -0,0 +1,23 @@
+//Methods:
+//Add users to Vector
+//User output
+//You can sort users by one criterion, for example by id number
+//
+//User Model :
+//You can add more fields to the user if you wish.
+//Class User{ id, first name, surname, phone number };
+//
+//CRUD:
+//1.  Create
+//2.  Read
+//3.  Update
+//4.  Delete
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	system("pause");
+	return 0;
+}
